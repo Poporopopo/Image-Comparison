69eaf26bb117f93f3e7be4a36eea762575a530a2
Poporopopo <poporopyon@gmail.com>
2020-12-28 23:01:48 -0500

    can compare images only if dimensions are the same

--- main.py
+++ main.py
@@ -1,70 +1,46 @@
 # code taken from https://www.pyimagesearch.com/2014/09/15/python-compare-two-images/
 
 # import the necessary packages
-from skimage.measure import structural_similarity as ssim
-import matplotlib.pyplot as plt
+from skimage.metrics import structural_similarity as ssim
 import numpy as np
 import cv2
 
 def mse(imageA, imageB):
-	# the 'Mean Squared Error' between the two images is the
-	# sum of the squared difference between the two images;
-	# NOTE: the two images must have the same dimension
-	err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
-	err /= float(imageA.shape[0] * imageA.shape[1])
+    # the 'Mean Squared Error' between the two images is the
+    # sum of the squared difference between the two images;
+    # NOTE: the two images must have the same dimension
+    err = np.sum((imageA.astype("float") - imageB.astype("float")) ** 2)
+    err /= float(imageA.shape[0] * imageA.shape[1])
 
-	# return the MSE, the lower the error, the more "similar"
-	# the two images are
-	return err
+    # return the MSE, the lower the error, the more "similar"
+    # the two images are
+    return err
 
-
-def compare_images(imageA, imageB, title):
-	# compute the mean squared error and structural similarity
-	# index for the images
-	m = mse(imageA, imageB)
-	s = ssim(imageA, imageB)
-
-    # setup the figure
-	fig = plt.figure(title)
-	plt.suptitle("MSE: %.2f, SSIM: %.2f" % (m, s))
-
-    # show first image
-	ax = fig.add_subplot(1, 2, 1)
-	plt.imshow(imageA, cmap = plt.cm.gray)
-	plt.axis("off")
-
-    # show the second image
-	ax = fig.add_subplot(1, 2, 2)
-	plt.imshow(imageB, cmap = plt.cm.gray)
-	plt.axis("off")
-
-    # show the images
-	plt.show()
+def compare_images(imageA, imageB):
+    # compute the mean squared error and structural similarity
+    # index for the images
+    # m = mse(imageA, imageB)
+    # return m
+    s = ssim(imageA, imageB)
+    return s
+    # return (f"{m},{s}")
 
 # load the images -- the original, the original + contrast,
 # and the original + photoshop
 original = cv2.imread("images/prinz eugen (azur lane) drawn by moyoron - 7a93c1324142c0edb61ceb1496646627.jpg")
 contrast = cv2.imread("images/prinz eugen (azur lane) drawn by moyoron - 9facf3f48edbea83feb51d350d304f54.jpg")
 shopped = cv2.imread("images/prinz eugen (azur lane) drawn by moyoron - bfb1f44395b74e7a1c8ffb31fa9b03d2.jpg")
+other = cv2.imread("images/illustrious (azur lane) drawn by zynxy - 163198c4d8d26a59422161123d58d246.jpg")
 
 # convert the images to grayscale
 original = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
 contrast = cv2.cvtColor(contrast, cv2.COLOR_BGR2GRAY)
 shopped = cv2.cvtColor(shopped, cv2.COLOR_BGR2GRAY)
+other = cv2.cvtColor(other, cv2.COLOR_BGR2GRAY)
 
-# initialize the figure
-fig = plt.figure("Images")
-images = ("Original", original), ("Contrast", contrast), ("Photoshopped", shopped)
-# loop over the images
-for (i, (name, image)) in enumerate(images):
-	# show the image
-	ax = fig.add_subplot(1, 3, i + 1)
-	ax.set_title(name)
-	plt.imshow(image, cmap = plt.cm.gray)
-	plt.axis("off")
-# show the figure
-plt.show()
 # compare the images
-compare_images(original, original, "Original vs. Original")
-compare_images(original, contrast, "Original vs. Contrast")
-compare_images(original, shopped, "Original vs. Photoshopped")
+print ("Comparing original with itself", compare_images(original, original))
+print ("Comparing original with contrast", compare_images(original, contrast))
+print ("Comparing original with the shopped", compare_images(original, shopped))
+# throws error since dimensions don't match
+# print ("Comparing original with completely different image", compare_images(original, other))
